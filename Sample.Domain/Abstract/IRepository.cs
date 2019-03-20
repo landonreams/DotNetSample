ca9d7d4b141d7e12b54659079a390cac566183b2
landonreams <me@landonreams.com>
2019-03-20 12:49:06 -0400

    Created Unit Test project; Added some Domain classes.

---
+++ Sample.Domain/Abstract/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Domain.Abstract
+{
+    public interface IRepository<T>
+    {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        IEnumerable<T> List();
+        IEnumerable<T> Query(Expression<Func<T, bool>> predicate);
+
+        Task<IEnumerable<T>> ListAsync();
+        Task<IEnumerable<T>> QueryAsync(Expression<Func<T, bool>> predicate);        
+    }
+}
---
+++ Sample.Domain/Entities/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Domain.Entities
+{
+    public class Message
+    {
+        public uint Id { get; }
+        public string Text { get; }
+
+        public Message(uint id, string text)
+        {
+            Id = id;
+            Text = text;
+        }
+    }
+}
