015f4838f8507dde335d8c75aeda719ebe4d0b9c
landonreams <me@landonreams.com>
2019-03-20 17:43:31 -0400

    Committing today's progress.
    Sample.API - Implemented basic API and added SimpleInjector DI. Waiting on a response before finalizing - the requirements are unclear on whether the API should return Hello World 10x or once.
    
    Sample.Domain - Added IMessageRepository, IMessageService, IRepository, and Message
    
    Sample.Infrastructure - Created a simple MessageRepository based on an in-memory dictionary.

---
+++ Sample.Domain/Abstract/IMessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using Sample.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample.Domain.Abstract
+{
+    public interface IMessageRepository : IRepository<Message>
+    {
+        Task<Message> FindByIdAsync(Guid id);
+    }
+}
